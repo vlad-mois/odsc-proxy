b0dd806a9c678f8812afbe62d63944a73a6a11e9
vlad-mois <ijlil@yandex.ru>
2022-05-26 01:58:30 +0300

    Fill function body and requirements

--- predict/__init__.py
+++ predict/__init__.py
@@ -1,24 +1,35 @@
-import logging
-
 import azure.functions as func
+import json
+import os
+import psycopg2
+import psycopg2.extras
+import requests
+from typing import List
+
+
+def _get_predictions(reviews: List[str]) -> List[str]:
+    response = requests.post(os.getenv('MODEL_URL'), json=reviews)
+    response.raise_for_status()
+    return json.loads(response.json())
+
+
+def _db_save(reviews: List[str], predictions: List[str]) -> None:
+    conn = psycopg2.connect(
+        host=os.getenv('POSTGRES_HOST'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASS'),
+        dbname='postgres'
+    )
+    psycopg2.extras.execute_values(
+        conn.cursor(),
+        'INSERT INTO reviews (review, prediction) VALUES %s',
+        zip(reviews, predictions)
+    )
+    conn.commit()
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    reviews: List[str] = req.get_json()
+    predictions: List[str] = _get_predictions(reviews)
+    _db_save(reviews, predictions)
+    return func.HttpResponse(json.dumps(predictions))
